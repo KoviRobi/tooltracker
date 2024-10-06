91f8834908a189acb269eda5e3a0a7cb7de5e66b
Kovacsics Robert <kovirobi@gmail.com>
2024-10-06 14:57:27 +0100

    Add nix flake

---
+++ flake.nix
@@ -0,0 +1,16 @@
+{
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          packages = [
+            pkgs.go
+            pkgs.gopls
+            pkgs.sqlite
+          ];
+        };
+      });
+}
