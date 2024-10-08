b610d90f00f0ecc1768d09f9eaeefb12f8d1a555
Kovacsics Robert <kovirobi@gmail.com>
2024-10-08 01:36:27 +0100

    Add package to flake

--- flake.nix
+++ flake.nix
@@ -18,6 +18,18 @@
             pkgs.sqlite
           ];
         };
+
+        packages.default = self.packages.${system}.tooltracker;
+        packages.tooltracker = pkgs.buildGoModule {
+          pname = "tooltracker";
+          version = self.rev or "unstable${builtins.substring 0 8 self.lastModifiedDate}";
+
+          src = "${self}";
+
+          vendorHash = "sha256-ncuaR7JGJBEhXH285mG0V6oLlGul7jUyqT2+oVwWAuE=";
+
+          subPackages = [ "cmd/tooltracker.go" ];
+        };
       }
     );
 }
