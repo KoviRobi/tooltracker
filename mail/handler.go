3058ebfa506f4263348cda12dcf2024d7bc17403
Kovacsics Robert <kovirobi@gmail.com>
2025-01-26 14:27:38 +0000

    go: Split mail parsing into separate module from SMTP

---
+++ mail/handler.go
@@ -0,0 +1,168 @@
+package mail
+
+import (
+	"bytes"
+	"errors"
+	"io"
+	"log"
+	"net"
+	"regexp"
+	"strings"
+
+	"github.com/KoviRobi/tooltracker/db"
+	"github.com/emersion/go-msgauth/dkim"
+	"github.com/k3a/html2text"
+	"github.com/mcnijman/go-emailaddress"
+	"github.com/mnako/letters"
+)
+
+// Data passed around during the processing of a single mail
+type Session struct {
+	Db   db.DB
+	Dkim string
+	From *string
+}
+
+var InvalidError = errors.New("Invalid email")
+
+var verifyOptions = dkim.VerifyOptions{
+	LookupTXT: net.LookupTXT,
+}
+
+var borrowRe = regexp.MustCompile(`^(?i)Borrowed[ +](.*)$`)
+
+// Handle "Re:" and other localised versions
+// TODO: Non-ASCII?
+var aliasRe = regexp.MustCompile(`^(?i)(\w*:\s*)?Alias([ +].*)?\b`)
+
+const multipartPrefix = "multipart/"
+const contentType = "Content-Type"
+const contentTransferEncoding = "Content-Transfer-Encoding"
+const maxPartBytes = 10 * 1024
+const maxMessageBytes = 1024 * 1024
+const maxRecipients = 10
+
+func (s *Session) Handle(r io.Reader) error {
+	buf := make([]byte, maxMessageBytes)
+	n, err := r.Read(buf)
+	if n == 0 && err != nil {
+		log.Println(err)
+		return InvalidError
+	}
+	reader := bytes.NewReader(buf[:n])
+
+	if s.From == nil {
+		log.Println("No from in session")
+		return InvalidError
+	}
+
+	delegate := s.Db.GetDelegatedEmailFor(*s.From)
+
+	if s.Dkim != "" {
+		dkimDomain := s.Dkim
+		// At this point, we must have set an alias delegate using DKIM valid alias
+		// command
+		if *s.From != delegate {
+			address, err := emailaddress.Parse(*s.From)
+			if err != nil {
+				log.Println(err)
+				return InvalidError
+			}
+			dkimDomain = address.Domain
+		}
+		reader.Seek(0, io.SeekStart)
+		verifications, err := dkim.VerifyWithOptions(reader, &verifyOptions)
+		if err != nil {
+			log.Println(err)
+			return InvalidError
+		}
+
+		verified := false
+		for _, verification := range verifications {
+			if verification != nil {
+				if verification.Err == nil {
+					if verification.Domain == dkimDomain {
+						verified = true
+					} else {
+						log.Printf("Verified %s but not the one we are looking for: %s\n",
+							verification.Domain, dkimDomain)
+					}
+				} else {
+					log.Printf("Failed to verify %s: %s\n", verification.Domain, verification.Err)
+				}
+			}
+		}
+		if !verified {
+			log.Println("Failed to verify message")
+			return InvalidError
+		}
+	}
+
+	reader.Seek(0, io.SeekStart)
+	m, err := letters.ParseEmail(reader)
+	if err != nil {
+		log.Println(err)
+		return InvalidError
+	}
+
+	subject := m.Headers.Subject
+	body := m.Text
+	if body == "" {
+		body = html2text.HTML2Text(m.HTML)
+	}
+	if borrow := borrowRe.FindStringSubmatch(subject); borrow != nil {
+		return s.processBorrow(body, borrow[1])
+	} else if alias := aliasRe.FindStringSubmatch(subject); alias != nil {
+		// Only set up delegates from the DKIM validated email, to prevent chains of
+		// delegates
+		var delegates *string
+		if *s.From == delegate {
+			delegates = &alias[2]
+		}
+		return s.processAlias(body, delegates)
+	} else {
+		log.Println("Bad command", subject)
+		return InvalidError
+	}
+}
+
+func (s *Session) processBorrow(body, borrow string) error {
+	if s.FromRe.FindStringIndex(*s.From) == nil {
+		go s.notifyAliasSetup(*s.From, s.To)
+	}
+
+	body = strings.TrimSpace(body)
+	comment := strings.SplitN(body, "\n", 2)[0]
+	comment = strings.TrimSpace(comment)
+	location := db.Location{
+		Tool:       borrow,
+		LastSeenBy: *s.From,
+		Comment:    &comment,
+	}
+	s.Db.UpdateLocation(location)
+
+	return nil
+}
+
+func (s *Session) processAlias(body string, delegateFrom *string) error {
+	body = strings.TrimSpace(body)
+	alias := strings.SplitN(body, "\n", 2)[0]
+	alias = strings.TrimSpace(alias)
+	s.Db.UpdateAlias(db.Alias{
+		Email: *s.From,
+		Alias: alias,
+	})
+
+	if delegateFrom != nil {
+		from := emailaddress.FindWithRFC5322([]byte(*delegateFrom), false)
+		for _, address := range from {
+			s.Db.UpdateAlias(db.Alias{
+				Email:          address.String(),
+				Alias:          alias,
+				DelegatedEmail: s.From,
+			})
+		}
+	}
+
+	return nil
+}
