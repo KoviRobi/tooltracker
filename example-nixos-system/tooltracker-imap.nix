701e1f4d8e90723f2c692ed33a441cc2d3d2ecf0
Kovacsics Robert <kovirobi@gmail.com>
2025-01-30 10:23:40 +0000

    nix: Copy tooltracker-{smtp,imap}.nix

---
+++ example-nixos-system/tooltracker-imap.nix
@@ -0,0 +1,40 @@
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
+let
+  domain = "tooltracker-proto.co.uk";
+in
+{
+  services = {
+    tooltracker = {
+      inherit domain;
+      enable = true;
+      listen = "0.0.0.0";
+      smtp = {
+        enable = true;
+        port = 25;
+      };
+      from = ".*";
+    };
+
+    httpd = {
+      enable = true;
+
+      virtualHosts.${domain} = {
+        locations."/" = {
+          proxyPass = with config.services.tooltracker; "http://${listen}:${toString http-port}/";
+        };
+      };
+    };
+
+    sshd.enable = true;
+  };
+
+  networking.firewall.allowedTCPPorts = [
+    80
+    443
+  ];
+}
