b26f70cd2b442419de45794a91d736a3fc689d51
Kovacsics Robert <kovirobi@gmail.com>
2025-03-19 00:08:26 +0000

    deploy: Parametrize login hint; improve create token message

--- example-nixos-system/tooltracker-imap.nix
+++ example-nixos-system/tooltracker-imap.nix
@@ -8,6 +8,7 @@
 let
   pizauth = lib.getExe pkgs.pizauth;
   logger = "${pkgs.util-linux}/bin/logger";
+  cfg = config.services.tooltracker;
 in
 {
   environment.systemPackages = [
@@ -53,7 +54,7 @@ in
       config = pkgs.writeText "pizauth.conf" ''
         auth_notify_cmd = "${logger} -t tooltracker 'Visit $PIZAUTH_URL'";
 
-        // TODO: Create new Entra token
+        // TODO (end user): Create new Entra token if you want a custom redirect URI
         account "tooltracker" {
             auth_uri = "https://login.microsoftonline.com/common/oauth2/v2.0/authorize";
             token_uri = "https://login.microsoftonline.com/common/oauth2/v2.0/token";
@@ -62,7 +63,7 @@ in
               "https://outlook.office365.com/IMAP.AccessAsUser.All",
               "offline_access"
             ];
-            auth_uri_fields = { "login_hint": "robertkovacsics@carallon.com" };
+            auth_uri_fields = { "login_hint": "${cfg.imap.user}" };
             redirect_uri = "http://localhost";
         }
       '';
